61cbe852de321e6cb10f53bb5a1079d54efc5552
mostafa mohamed <mmm654654mmm@gmail.com>
2025-06-30 04:03:06 -0700

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,34 @@
+# syntax =>{variadle}
+
+# name convetion and rules
+# 1. variable name should not start with number
+# 2. variable name should not contain special characters except underscore(_)   
+# 3. variable name should not contain space
+# 4. variable name should not be a keyword or reserved word in python
+# 5. variable name should be descriptive and meaningful
+# 6. variable name should be in lowercase, if it contains multiple words, use underscore    
+# 7. variable name should not be too long or too short
+# 8. variable name should not be a built-in function or module name
+# 9. variable name should not be a class name
+# 10. variable name should not be a constant name (all uppercase)
+
+# example 
+
+name =input("enter your name:")
+age = input("enter your age:")
+
+
+
+
+print("hello",name,"your age is " + age)
+
+
+# c = x = g = "hello"
+
+# print(c)
+# print(x)
+# print(g)
+
+
+
+
